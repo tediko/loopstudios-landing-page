df94b2461f6565478e0caeb9a03612bc9fa72c1f
tediko <tediko123@wp.pl>
2021-04-22 18:41:28 +0200

    Added stickyNav

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 import ToggleMenu from './toggleMenu.js';
+import LazyAnimations from './lazyAnimations.js';
+import StickyNav from './stickyNav.js';
+
 new ToggleMenu();
+new LazyAnimations();
+new StickyNav();
---
+++ js/stickyNav.js
@@ -0,0 +1,84 @@
+export default class StickyNav {
+    constructor() {
+        if (!this.vars()) return false;
+        this.setupEvents();
+    }
+
+    vars() {
+        this.selectors = {
+            header: 'data-header',
+            sectionKv: 'data-kv',
+            activeClass: 'nav-scroll'
+        }
+
+        this.header = document.querySelector(`[${this.selectors.header}]`);
+        this.sectionKv = document.querySelector(`[${this.selectors.sectionKv}]`);
+        if (!this.header || !this.sectionKv) return false;
+
+        this.isInitialized = false;
+
+        return true;
+    }
+
+    setupEvents() {
+        this.navObserver(this.sectionKv);
+    }
+
+    navObserver(element) {
+        this.options = {
+            rootMargin: '0px',
+            threshold: 0.1
+        }
+        
+        this.animations = {
+            fadeIn: [
+                {
+                    opacity: 0,
+                    transform: 'translateY(-62px)',
+                },
+                {
+                    opacity: 1,
+                    transform: 'translateY(0)',
+                }
+            ],
+            fadeOut: [
+                {
+                    transform: 'translateY(0)',
+                    opacity: 1,
+                },
+                {
+                    opacity: 0,
+                    transform: 'translateY(-62px)',
+                }
+            ]
+        };
+
+        this.observer = new IntersectionObserver(entries => {
+            if (this.isInitialized) {
+                entries.forEach(entry => {
+                    if (!entry.isIntersecting) {
+                        this.animNavIn = this.header.animate(this.animations.fadeIn, {
+                            duration: 600,
+                            fill: 'forwards'
+                        });
+
+                        this.header.classList.add(`${this.selectors.activeClass}`);
+                        this.sectionKv.classList.add(`${this.selectors.activeClass}`);
+                    } else {
+                        this.animNavOut = this.header.animate(this.animations.fadeOut, {
+                            duration: 300,
+                        });
+
+                        this.animNavOut.onfinish = () => {
+                            this.header.classList.remove(`${this.selectors.activeClass}`);
+                            this.sectionKv.classList.remove(`${this.selectors.activeClass}`);
+                        }
+                    }
+                })
+            }
+            this.isInitialized = true;
+        }, this.options)
+
+        this.observer.observe(element);
+    }
+}
